d3f6968a35f45a5955686c225e8c9e18329df1f0
ebrahimnigus <ebrahimnigus9@gmail.com>
2024-12-20 10:04:10 +0300

    add images and thumbnails

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './TitleCards'
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
   return (
-    <div className="titleCards"></div>
+    <div className="titleCards">
+    <h2>Popular On Netflix</h2>
+      <div className='card-list'>
+        {cards_data.map((card, index)=>{
+          return <div className='card' key={index}>
+            <img src={card.image} alt=''/>
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
