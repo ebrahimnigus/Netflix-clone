43bb2bdf1996158e11aa16434ab9579a30d6b9cc
ebrahimnigus <ebrahimnigus9@gmail.com>
2024-12-21 09:49:48 +0300

    add footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../Components/TitleCards/TitleCards'
+import Footer from '../../Components/Footer/Footer'
 const Home = () => {
   return (
     <div className="home">
@@ -22,6 +23,13 @@ const Home = () => {
             <TitleCards/>
           </div>
         </div>
+        <div className="more-cards">
+          <TitleCards title={"Blockbuster Movies"}/>
+          <TitleCards title={"Only on Netflix"}/>
+          <TitleCards title={"Upcoming"}/>
+          <TitleCards title={"Topics for You"}/>
+        </div>
+        <Footer />
     </div>
   )
 }
