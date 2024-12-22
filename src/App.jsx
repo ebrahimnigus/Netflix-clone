ec618121581bf190401bf6fed098e3fda18fc64c
ebrahimnigus <ebrahimnigus9@gmail.com>
2024-12-22 11:55:25 +0300

    fetch video data and link to home thumbnails

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
-        
+        <Route path='/player/:id' element={<Player/>}/>
       </Routes>
     </div>
   )
